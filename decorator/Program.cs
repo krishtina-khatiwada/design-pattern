62b8ea77e87ad337564ee4035f2f014313db22f0
krishtina-khatiwada <krishtina.khatiwada@truenary.com>
2025-05-29 17:46:51 +0545

    chore:new design pattern decorator

---
+++ decorator/Program.cs
@@ -0,0 +1,57 @@
+﻿interface IMario
+{
+    string Getabilities();
+}
+class PlainMario : IMario
+{
+    public string Getabilities()
+    {
+        return "Mario";
+    }
+}
+class MarioDecorator : IMario
+{
+    protected IMario mario;
+    public MarioDecorator(IMario mario)
+    {
+        this.mario = mario;
+    }
+    public virtual string Getabilities()
+    {
+    return mario.Getabilities();
+    }
+}
+
+class SuperMario : MarioDecorator
+{
+    public SuperMario(IMario mario) : base(mario) { }
+    public override string Getabilities()
+    {
+        return base.Getabilities() + " Big size ";
+    }
+}
+class WithGUN : MarioDecorator
+{
+    public WithGUN(IMario mario) : base(mario) { }
+    public override string Getabilities()
+    {
+        return base.Getabilities() + " With Gun ";
+    }
+}
+class Flower : MarioDecorator {
+    public Flower (IMario mario) : base(mario) { }
+    public override string Getabilities()
+    {
+        return base.Getabilities() + " has flower ";
+    }
+}
+class Program {
+    static void Main()
+    {
+        IMario mario = new PlainMario();
+        mario = new SuperMario(mario);
+        mario = new Flower(mario);
+        mario = new WithGUN(mario);
+        System.Console.WriteLine(mario.Getabilities());
+    }
+}
