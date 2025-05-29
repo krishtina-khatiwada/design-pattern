ce0acdc0529f5eacf9d95ece1b939fc92ec56369
krishtina-khatiwada <krishtina.khatiwada@truenary.com>
2025-05-29 12:55:32 +0545

    chore: added singleton

---
+++ singleton/dosomething/Program.cs
@@ -0,0 +1,26 @@
+﻿class Singleton
+{
+    private static Singleton instance;
+    private Singleton() { }
+    public static Singleton Getinstance()
+    {
+        if (instance == null)
+        {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+    public void Dosomething()
+    {
+        Console.WriteLine("insantation through a singleton");
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Singleton ab = Singleton.Getinstance();
+        ab.Dosomething();
+    }
+    
+}
---
+++ singleton/threadsafe/Program.cs
@@ -0,0 +1,22 @@
+﻿class Singleton
+{
+    private static readonly Lazy<Singleton> lazyinstance= new Lazy<Singleton>(() => new Singleton());
+    private Singleton() { }
+    public static Singleton Getinstance()
+    {
+        return lazyinstance.Value;
+    }
+    public void Dosomething()
+    {
+        Console.WriteLine("insantation through a singleton");
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Singleton ab = Singleton.Getinstance();
+        ab.Dosomething();
+    }
+    
+}
