35f20cd533310c018cbfcc8a3021dd2b66c3416e
krishtina-khatiwada <krishtina.khatiwada@truenary.com>
2025-06-01 11:08:27 +0545

    chore:new file iterator added

---
+++ iterator/nameiterator/Program.cs
@@ -0,0 +1,36 @@
+﻿class NameCollection
+{
+    private string[] names = { "ram", "hari", "shyam" };
+    public NameIterator GetIterator()
+    {
+        return new NameIterator(names);
+    }
+}
+class NameIterator {
+    private string[] _names;
+    private int _position = 0;
+    public NameIterator(string[] names)
+    {
+        _names = names;
+    }
+    public bool Hasnext()
+    {
+        return _position < _names.Length;
+    }
+    public string Next()
+    {
+        return _names[_position++];
+    }
+}
+class Program {
+    static void Main()
+    {
+        NameCollection nameCollection = new NameCollection();
+        NameIterator nameIterator = nameCollection.GetIterator();
+        while (nameIterator.Hasnext())
+        {
+            System.Console.WriteLine(nameIterator.Next());
+        }
+
+    }
+}
